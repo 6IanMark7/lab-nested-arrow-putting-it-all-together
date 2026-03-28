9c2ac513648e46b0b050e40af1f100198f79fd1e
Ian <6markian7@gmail.com>
2026-03-28 20:57:02 +0300

    first commit with nested function and to-finish arrow functio

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-
+//LOGN FEATURE CODE USING NESTED AND ARROW FUNCTIONS 
 
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+//USER DATA(OBJECT)
+
+const userData =
+  {
+    username: "John Doe",
+    password: "whoami254"
+}
+  
+//NESTED LOGIN FUNCTION
+function createLoginTracker(userInfo) {
+  console.log("Welcome to the JavaScript Functions Lab app");
+
+    const userName = prompt("Input your user name");
+    const userPass = prompt("Input your password");
+  
+  let attemptCount = 0;
+  const arrowFunc = (passwordAttempt) => {
+     
+  }
+}
